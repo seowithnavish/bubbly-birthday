fb802a964896e80e23615953e0dd2fc2dd2cdfad
Navish Qamar <officialnavish63@gmail.com>
2026-07-08 19:36:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.addEventListener('mousemove', (e) => {
 });
 
 // Typing effect for greeting
-const greetingText = "Hey You Know What! You're the most adorable human i ever met! 💖";
+const greetingText = "Hey You Know What! You're the most adorable person i ever met! 💖";
 const greetingElement = document.querySelector('.greeting');
 let charIndex = 0;
 
@@ -88,7 +88,7 @@ window.addEventListener('load', () => {
                 opacity: 0,
                 duration: 1,
                 onComplete: () => {
-                    window.location.href = 'cake.html'; // Now leads to the new Birthday Cake section before the reasons page
+                    window.location.href = 'cause.html'; // Replace with the actual URL of the next page
                 }
             });
         });
